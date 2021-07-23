9b961ac73db0bccf0a6a75661b7597f832417a4d
frangersal <programacion.practicas.1@hidrogeno8.com>
2021-07-23 14:35:30 -0500

    first commit

---
+++ promedioPonderado/js/promedio.js
@@ -0,0 +1,72 @@
+const mexico = [
+    {
+        nombre: "Francisco",
+        salario: 1000,
+    },
+    {
+        nombre: "Alina",
+        salario: 900,
+    },
+    {
+        nombre: "Gerardo",
+        salario: 500,
+    },
+    {
+        nombre: "Salinas",
+        salario: 10,
+    },
+    {
+        nombre: "Mendoza",
+        salario: 10,
+    },
+];
+
+const chile = [
+    {
+        nombre: "Francisco",
+        salario: 1000,
+    },
+    {
+        nombre: "Alina",
+        salario: 900,
+    },
+    {
+        nombre: "Gerardo",
+        salario: 500,
+    },
+    {
+        nombre: "Salinas",
+        salario: 10,
+    },
+    {
+        nombre: "Mendoza",
+        salario: 10,
+    },
+];
+
+const colombia = [
+    {
+        nombre: "Francisco",
+        salario: 1000,
+    },
+    {
+        nombre: "Alina",
+        salario: 900,
+    },
+    {
+        nombre: "Gerardo",
+        salario: 500,
+    },
+    {
+        nombre: "Salinas",
+        salario: 10,
+    },
+    {
+        nombre: "Mendoza",
+        salario: 10,
+    },
+];
+
+function calcularMedia(params) {
+    
+}
